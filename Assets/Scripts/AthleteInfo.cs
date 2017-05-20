531e8fa6a205aed7f999d571277d5758ff9287a1
kikoncuo <enriqueag26@gmail.com>
2017-05-20 12:24:30 +0200

    Instantiate athletes at runtime

---
+++ Assets/Scripts/AthleteInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AthleteInfo : MonoBehaviour {
+    [HideInInspector]
+    public Athlete athleteInfo;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/Scripts/Initializer.cs
+++ Assets/Scripts/Initializer.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 //Loads the info of the players from a Json
 public class Initializer : MonoBehaviour {
 
+    public GameObject athlete;
+    public GameObject athleteContainer;
     public TextAsset jsonFile;
 
     // Use this for initialization
     void Start () {
-        AthleteArray athletes = JsonUtility.FromJson<AthleteArray>(jsonFile.text);
+        AthleteArray athletesArray = JsonUtility.FromJson<AthleteArray>(jsonFile.text);
+        for (int i = 0; i < athletesArray.athletes.Length; i++)
+        {
+            GameObject intstantiatedAthlete = Instantiate(athlete);
+            intstantiatedAthlete.transform.parent = athleteContainer.transform;
+            intstantiatedAthlete.GetComponent<AthleteInfo>().athleteInfo = athletesArray.athletes[i];
+        }
 	}
 	
 	// Update is called once per frame
